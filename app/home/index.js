65a8ffe939da766aac14b0241a57c9b9fe127049
borczgrzegorz <borcz.grzegorz@gmail.com>
2016-05-26 11:56:55 +0000

    User search view

--- app/home/index.js
+++ app/home/index.js
@@ -2,10 +2,14 @@ import angular from 'angular';
 import uirouter from 'angular-ui-router';
 import routing from './home.routes';
 
+//directives
+import userInput from './directives/user_input';
+
 //Controller
 import TabController from './tab.controller';
 
 export default angular.module('app.home',[uirouter])
 .config(routing)
 .controller('TabController',TabController)
+.directive('userInput', userInput)
 .name;
