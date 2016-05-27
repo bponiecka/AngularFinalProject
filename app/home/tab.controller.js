78c0f1ebcf5d7dbbea51e773ef74675dcd8fc092
borczgrzegorz <borcz.grzegorz@gmail.com>
2016-05-27 10:33:40 +0000

    add and remove tabs without firebase

--- app/home/tab.controller.js
+++ app/home/tab.controller.js
@@ -1,5 +1,32 @@
 export default class TabController{
-    constructor($stateParams){
+    constructor($scope,$stateParams){
+        this.todos = [];
         this.user = $stateParams.user
+        this.userText = '';
+    }
+    
+    addTodo()
+    {
+        this.todos.push({
+            user: this.user,
+            title: this.userText,
+            completed:false
+        });
+    }
+    
+    onUser()
+    {
+        this.selectedTab = 'user';
+    }
+    
+    onAll()
+    {
+        this.selectedTab = 'all';
+    }
+    
+    removeTodo(toDo)
+    {
+        var index = this.todos.indexOf(toDo);
+        this.todos.splice(index,1);
     }
 }
